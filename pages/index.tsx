8e86c6bc841130865e6cbc395eeda325e56733cb
Martin Tudor <martcan27@gmail.com>
2021-09-05 16:51:55 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({ cardData }: { cardData: any }) {
                   alt=""
                   width={196}
                   height={160}
-                  layout="responsive"
+                  layout="fixed"
                   objectFit="cover"
                 />
                 <a href={card.link}>
