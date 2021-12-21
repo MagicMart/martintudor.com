1ee2f18006388c6143c93f7958419966bf5b9210
Martin Tudor <martcan27@gmail.com>
2021-12-21 16:08:18 +0000

    add about page

--- components/Page.tsx
+++ components/Page.tsx
@@ -7,16 +7,6 @@ export default function Page({ children }: { children: React.ReactNode }) {
       <Header />
       <main>{children}</main>
       <footer>
-        {/* <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a> */}
         <p>
           <a href="https://github.com/MagicMart">Martin Tudor</a>{' '}
           <span>&#169;</span> {new Date().getFullYear()}
--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
 const nextConfig = {
   /* config options here */
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dibbxe0wt/image/upload/',
   },
+  ...withMDX({
+    pageExtensions: ['js', 'jsx', 'tsx', 'md', 'mdx'],
+  }),
 }
 
 module.exports = nextConfig
