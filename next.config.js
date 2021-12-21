1918f6f77eb84e062776f9acc1c27cb8c2d24aad
Martin Tudor <martcan27@gmail.com>
2021-12-21 14:26:08 +0000

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
   images: {
     loader: 'cloudinary',
     path: 'https://res.cloudinary.com/dibbxe0wt/image/upload/',
   },
 }
+
+module.exports = nextConfig
