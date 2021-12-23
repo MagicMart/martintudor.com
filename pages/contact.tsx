a3510005608c6a10567e06302ded68b699cb26d3
MagicMart <martcan27@gmail.com>
2021-12-23 14:55:56 +0000

    htmlFor

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import ContactForm from '../components/ContactForm'
 
 export default function Contact() {
   return (
@@ -6,42 +7,7 @@ export default function Contact() {
       <Head>
         <title>Contact | martintudor.com</title>
       </Head>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>
-            Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-      <style jsx>{`
-        input {
-          width: 100%;
-          height: 3rem;
-        }
-        textarea {
-          width: 100%;
-        }
-        input,
-        textarea {
-          font-size: 2rem;
-        }
-        button {
-          font-size: 4rem;
-        }
-      `}</style>
+      <ContactForm />
     </>
   )
 }
