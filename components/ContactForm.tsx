a84201205e15361a8eae9cb85f5a6fd7607a4bf0
MagicMart <martcan27@gmail.com>
2021-12-23 15:14:11 +0000

    form element

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,42 +1,23 @@
-export default function ContactForm() {
-  return (
-    <>
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label htmlFor="name">
-            Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label htmlFor="email">
-            Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label htmlFor="message">
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-      <style jsx>{`
-        input {
-          width: 100%;
-          height: 3rem;
-        }
-        textarea {
-          width: 100%;
-        }
-        input,
-        textarea {
-          font-size: 2rem;
-        }
-        button {
-          font-size: 4rem;
-        }
-      `}</style>
-    </>
-  )
-}
+// export default function ContactForm() {
+//   return (
+//     <>
+
+//       <style jsx>{`
+//         input {
+//           width: 100%;
+//           height: 3rem;
+//         }
+//         textarea {
+//           width: 100%;
+//         }
+//         input,
+//         textarea {
+//           font-size: 2rem;
+//         }
+//         button {
+//           font-size: 4rem;
+//         }
+//       `}</style>
+//     </>
+//   )
+// }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,27 @@
 import Head from 'next/head'
-import ContactForm from '../components/ContactForm'
+
+const contactForm: any = `
+  <form name="contact" method="POST" data-netlify="true">
+    <p>
+      <label htmlFor="name">
+        Name: <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label htmlFor="email">
+        Email: <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <label htmlFor="message">
+        Message: <textarea name="message"></textarea>
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+`
 
 export default function Contact() {
   return (
@@ -7,7 +29,23 @@ export default function Contact() {
       <Head>
         <title>Contact | martintudor.com</title>
       </Head>
-      <ContactForm />
+      <div dangerouslySetInnerHTML={contactForm}></div>
+      <style jsx>{`
+        input {
+          width: 100%;
+          height: 3rem;
+        }
+        textarea {
+          width: 100%;
+        }
+        input,
+        textarea {
+          font-size: 2rem;
+        }
+        button {
+          font-size: 4rem;
+        }
+      `}</style>
     </>
   )
 }
