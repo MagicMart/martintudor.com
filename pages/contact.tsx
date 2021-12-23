a2fecddd682791384612170a7667d1a24a0b55ab
MagicMart <martcan27@gmail.com>
2021-12-23 14:08:53 +0000

    remove styles

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -26,22 +26,6 @@ export default function Contact() {
           <button type="submit">Send</button>
         </p>
       </form>
-      <style jsx>{`
-        input {
-          width: 100%;
-          height: 3rem;
-        }
-        textarea {
-          width: 100%;
-        }
-        input,
-        textarea {
-          font-size: 2rem;
-        }
-        button {
-          font-size: 4rem;
-        }
-      `}</style>
     </>
   )
 }
