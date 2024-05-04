0b06e408b80ab3f72949dc791bc167716c1b5f1d
MagicMart <martcan27@gmail.com>
2024-05-04 20:17:36 +0000

    update

--- components/MyHead.tsx
+++ components/MyHead.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function MyHead({ title }: { title: string }) {
+export function MyHead({ title }: { title: string }) {
   return (
     <Head>
       <title>{title}</title>
